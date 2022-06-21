b197872d0ce7aab210d617a85f943944110cee54
Elva Choi <avla@Elvas-MacBook-Pro-2.local>
2022-06-21 15:45:35 +0100

    first commit

---
+++ src/components/AccordionData.js
@@ -0,0 +1,12 @@
+export const AccordionData = [
+    {
+        id: 1,
+        title: "Sort Home Feed by Default",
+        content: ["Default", "Featured", "Latest", "Following", "Bookmarks"]
+    },
+    {
+        id: 2,
+        title: "More Things to do",
+        content: ["✍🏻 Write a post", "🎨 Design a site", "🚀 Ninja Training", "👓 Read Tutorials", "💵 Build a SAAS", "🙋 Ask a Question", "🍿 Watch Videos", "⭐ Upgrade to Pro"]
+    }
+]
